f5d3c4ba8395945877387f8a2aa44e0e6929c9c1
mohur <m.ashrahfi@gmail.com>
2020-10-24 22:35:12 -0700

    Adding jump_height and fixing bug where gravity keeps getting added

--- src/KinematicBody2D.gd
+++ src/KinematicBody2D.gd
@@ -1,20 +1,29 @@
 extends KinematicBody2D
 
-#Vector2() is an object that contains the x and y motions. 
+#Vector2() is an object that contains the x and y motions. Vector2(x, y) 
 var motion = Vector2() #contians the motion of the character. Now has access to motion.x and motion.y
-var speed = 200
-var gravity = 40
+const UP = Vector2(0, -1)
+const SPEED = 200
+const GRAVITY = 35
+var JUMP_HEIGHT = -650
 
 func _physics_process(delta):
-	motion.y = gravity
+	motion.y += GRAVITY #for every frame it ADDS 10 instead of hard coding 10. This allows for realistic reaction of gravity,
 	
 	if Input.is_action_pressed("ui_right"): 
-		motion.x = speed
+		motion.x = SPEED
 		
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -speed
+		motion.x = -SPEED
 		
 	else:
 		motion.x = 0
 		
-	motion = move_and_slide(motion)
+	if is_on_floor():
+		print("On floor.")
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT 
+			
+#docs mention 2nd argument of func move_and_slide for allowing jump in vector. Update motion with the return of the whole functon to stop from continuously adding gravity.
+	motion = move_and_slide(motion, UP) 
+	print(motion)
